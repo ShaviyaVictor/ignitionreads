57c9455a279c15ebe85236bb2b5458ad49d688a8
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-15 05:06:18 +0300

    Production  configuration child class

--- app/config.py
+++ app/config.py
@@ -2,3 +2,17 @@ class Config:
   '''
   General configuration parent class
   '''
+  
+  pass
+
+
+
+class ProdConfig(Config):
+  '''
+  Production  configuration child class
+  
+  Args:
+    Config: The parent configuration class with General configuration settings
+  '''
+
+  pass
