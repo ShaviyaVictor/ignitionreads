e7be3b72de5cb14c245bde9ca1eb0a7e2ebcfad1
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-15 18:27:27 +0300

    Create variables for the LoginForm

--- app/forms.py
+++ app/forms.py
@@ -13,7 +13,8 @@ class RegistrationForm(FlaskForm) :
   email = StringField(label = 'Email', validators=[DataRequired(), Email()])
   password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, max=16)])
   confirm_password = PasswordField(label='Confrim Password', validators=[DataRequired(), EqualTo('password')])
-  register = SubmitField(label='REGISTER')
+
+  submit = SubmitField(label='REGISTER')
 
 
 
@@ -22,7 +23,6 @@ class LoginForm(FlaskForm) :
   Creating variables to hold our inputs for the Login form
   '''
   username = StringField(label = 'Username', validators=[DataRequired(), Length(min=3, max=15)] )
-  email = StringField(label = 'Email', validators=[DataRequired(), Email()])
   password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, max=16)])
-  confirm_password = PasswordField(label='Confrim Password', validators=[DataRequired(), EqualTo('password')])
-  register = SubmitField(label='REGISTER')
+
+  submit = SubmitField(label='LOGIN')
