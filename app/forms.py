88ccfa9c2dcb4e48e30877724106604e7bd3408c
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-15 18:23:10 +0300

    Create the variable for the Registration form, now creating the variables for the Login form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 import email
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 
@@ -8,8 +9,20 @@ class RegistrationForm(FlaskForm) :
   '''
   Creating variables to hold our inputs
   '''
-  username
-  email
-  password
-  confirm_password
-  register
+  username = StringField(label = 'Username', validators=[DataRequired(), Length(min=3, max=15)] )
+  email = StringField(label = 'Email', validators=[DataRequired(), Email()])
+  password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, max=16)])
+  confirm_password = PasswordField(label='Confrim Password', validators=[DataRequired(), EqualTo('password')])
+  register = SubmitField(label='REGISTER')
+
+
+
+class LoginForm(FlaskForm) :
+  '''
+  Creating variables to hold our inputs for the Login form
+  '''
+  username = StringField(label = 'Username', validators=[DataRequired(), Length(min=3, max=15)] )
+  email = StringField(label = 'Email', validators=[DataRequired(), Email()])
+  password = PasswordField(label='Password', validators=[DataRequired(), Length(min=6, max=16)])
+  confirm_password = PasswordField(label='Confrim Password', validators=[DataRequired(), EqualTo('password')])
+  register = SubmitField(label='REGISTER')
