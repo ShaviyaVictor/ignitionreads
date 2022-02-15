def7752b9b99398fdf13e02355cd68a7eb939dd0
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-15 05:01:48 +0300

    Add the reads page in the Views.py file

--- app/views.py
+++ app/views.py
@@ -9,4 +9,15 @@ def index() :
   View root page function that returns the index page and its data
   '''
 
+
   return render_template('index.html')
+
+
+
+@app.route('/reads')
+def reads() :
+  '''
+  View reads page function that returns the reads page
+  '''
+
+  return render_template('reads.html')
