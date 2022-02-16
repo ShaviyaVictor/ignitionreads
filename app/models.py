a3cb22d0b248505c0f4dc196626ae91e990914ea
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-16 17:39:57 +0300

    Create a user model

--- app/models.py
+++ app/models.py
@@ -2,14 +2,14 @@ import email
 
 from click import password_option
 from app import db
-
+from datetime import datetime
 
 
 
 class User(db.Model) :
-  id
-  username
-  email
-  image_file
-  password
-  date_created
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(20), nullable=False)
+  date_created = db.Column(db.DateTime, default=datetime.utcnow)
