a5ae535933f9ff65e4318efbc4ad65f49e5d6942
ShaviyaVictor <shaviyavictor@gmail.com>
2022-02-18 09:39:53 +0300

    Make the string length to conform to the length of the generated password hash

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model) :
   username = db.Column(db.String(20), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-  password = db.Column(db.String(20), nullable=False)
+  password = db.Column(db.String(60), nullable=False)
   date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
